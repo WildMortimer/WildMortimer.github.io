3efeaae521cc62d14cf3373b788617fc977edd53
WildMortimer <eric.breyer@gmail.com>
2021-10-28 11:28:08 -0500

    connect the nodes?

--- ipfs/index.js
+++ ipfs/index.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', async () => {
       config: {
           Addresses: {
               Swarm: [
-                  '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
+                  '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+                  '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+                  '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
               ]
           },
           Bootstrap: []
