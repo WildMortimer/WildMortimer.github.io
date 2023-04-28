a3247f5a2a4211ecfe196135f8f30722c19260ac
WildMortimer <eric.breyer@gmail.com>
2023-04-28 12:47:50 -0500

    hm

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -26,7 +26,9 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
+  mode: 'hash',
   routes
 })
 
